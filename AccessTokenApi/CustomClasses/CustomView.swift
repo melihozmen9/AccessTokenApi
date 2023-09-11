0b468c6296b5d1846c79b329098394e84bcda86d
melihozmen9 <melihozmen9@gmail.com>
2023-09-11 11:13:50 +0300

    hi

--- AccessTokenApi/CustomClasses/CustomView.swift
+++ AccessTokenApi/CustomClasses/CustomView.swift
@@ -47,6 +47,7 @@ class CustomView: UIView {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+        print("merhaba")
     }
     
     private func setupView() {
